8f8d71b27d8eca00aa5614acae9bf19f9cf54d5a
earth <1581790391@qq.com>
2020-11-20 23:34:42 +0800

    code update

--- phpinfo.php
+++ phpinfo.php
@@ -1,3 +1,10 @@
 <?php
-echo "phpinfo();"
-?>
+$host="127.0.0.1";
+$userName="test";
+$password="Abc123456.";
+if($connID=mysqli_connect($host,$userName,$password)) {
+    echo "<script type='text/javascript'>alert('数据库连接成功');</script>\n";
+}else{
+    echo "<script type='text/javascript'>alert('数据库连接失败');</script>\n";
+}
+mysqli_query($connID,"set names utf8");
